c447c686bd8f08f9fd03e3f035bda0334e32e4d1
Kamus <misdarch@gmail.com>
2026-05-05 19:38:20 +0800

    v1

---
+++ sw.js
@@ -0,0 +1,31 @@
+const CACHE_NAME = 'gaple-cache-v1';
+const urlsToCache = [
+  './index.html',
+  './manifest.json'
+  // Jika kamu punya gambar icon, tambahkan di sini:
+  // './icon.png',
+  // './icon-512.png'
+];
+
+// Saat aplikasi pertama kali dijalankan, simpan file ke dalam memori (Offline)
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Saat aplikasi dibuka lagi, ambil dari memori agar lebih cepat dan bisa offline
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
